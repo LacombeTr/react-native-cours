a4fa674306bd17b24f10d3d782ab4d2910d06c93
Tristan Lacombe <tlacombevulca@outlook.fr>
2026-01-06 14:51:11 +0100

    fix(app): exercice-2 Corrected gradient size

--- my-weather-app/components/GradientBg.jsx
+++ my-weather-app/components/GradientBg.jsx
@@ -5,8 +5,8 @@ export const GradientBg = () => {
     return (
         <LinearGradient
             start={{ x: 0, y: 1 }}
-            end={{ x: 0, y: 0.6 }}
-            colors={["rgba(255, 255, 255, 1)","rgba(150, 216, 245, 1)"]}
+            end={{ x: 0, y: 0.4 }}
+            colors={["rgba(255, 255, 255, 1)", "rgba(150, 216, 245, 1)"]}
             style={styles.background}
         />
     );
@@ -14,10 +14,7 @@ export const GradientBg = () => {
 
 const styles = StyleSheet.create({
     background: {
-        position: "absolute",
-        left: 0,
-        right: 0,
-        top: 0,
-        height: "100%",
+        ...StyleSheet.absoluteFillObject,
+        zIndex: -1,
     },
 });
