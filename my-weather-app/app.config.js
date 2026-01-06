393f12934f16649a9415a32291ea79a0fdf0eb03
Tristan Lacombe <tlacombevulca@outlook.fr>
2026-01-06 10:59:19 +0100

    feat(app): exercice-2 Initial commit

---
+++ my-weather-app/app.config.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+export default {
+    expo: {
+        name: "My weather app",
+        slug: "my-weather-app",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        userInterfaceStyle: "light",
+        splash: {
+            image: "./assets/splash.png",
+        },
+    },
+};
