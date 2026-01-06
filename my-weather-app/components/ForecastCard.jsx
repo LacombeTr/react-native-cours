0956dcc546ddd08c2df6407db6b664f3c97e3fe6
Tristan Lacombe <tlacombevulca@outlook.fr>
2026-01-06 15:32:24 +0100

    refactor(app): exercice-2 Changed forecast card bg color

--- my-weather-app/components/ForecastCard.jsx
+++ my-weather-app/components/ForecastCard.jsx
@@ -24,7 +24,7 @@ export const ForecastCard = ({ instant }) => {
 const styles = StyleSheet.create({
     card: {
         width: 150,
-        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        backgroundColor: "rgba(0, 0, 0, 0.1)",
         borderRadius: 12,
         padding: 12,
         justifyContent: "center",
