17569c2fa8a6a4d9489f81f530666d56c5b21c44
Tristan Lacombe <tlacombevulca@outlook.fr>
2026-01-06 11:40:12 +0100

    feat(app): exercice-2 Added UseLocation hook

--- my-weather-app/App.js
+++ my-weather-app/App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { UseLocation } from "./hooks/useLocation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* <Text>Your API key is: {process.env.EXPO_PUBLIC_OPEN_WEATHER_API_KEY}</Text> */}
-      <StatusBar style="auto" />
-    </View>
-  );
+    const { location, errorMsg, adress } = UseLocation();
+
+    console.log(adress)
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>
+                Location:{" "}
+                {location ?  `adress: ${adress.city}, ${adress.region}, ${adress.country}` : "Fetching location..."}
+            </Text>
+            {/* <Text>Your API key is: {process.env.EXPO_PUBLIC_OPEN_WEATHER_API_KEY}</Text> */}
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ my-weather-app/hooks/useLocation.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import * as Location from "expo-location";
+
+export const UseLocation = () => {
+    const [location, setLocation] = useState(null);
+    const [adress, setAddress] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        async function getCurrentLocation() {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        }
+
+        getCurrentLocation();
+    }, []);
+
+    useEffect(() => {
+        async function getCityFromCoords(latitude, longitude) {
+            const result = await Location.reverseGeocodeAsync({
+                latitude,
+                longitude,
+            });
+
+            if (result.length > 0) {
+                const place = result[0];
+
+                setAddress({
+                    city: place.city,
+                    region: place.region,
+                    country: place.country,
+                    postalCode: place.postalCode,
+                });
+            }
+        }
+
+        console.log("Location changed:", location);
+        
+        if (location && location.coords) {
+            getCityFromCoords(
+                location.coords.latitude,
+                location.coords.longitude
+            );
+        }
+    }, [location]);
+
+    return {
+        location,
+        errorMsg,
+        adress,
+    };
+};
